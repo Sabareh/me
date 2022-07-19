95fc00e71b44ca9bd833321344635385a7e99e75
Sabare Victor Oketch <40304396+Sabareh@users.noreply.github.com>
2022-07-19 14:29:01 +0300

    dark

---
+++ pages/_document.tsx
@@ -0,0 +1,14 @@
+import {Html, Head, Main, Nextscript} from 'next/document'
+
+export default function Document() {
+    return (
+        <Html classname="dark">
+            <Head>      
+            </Head>
+            <body>
+                <Main/>
+                <Nextscript/>
+            </body>
+        </Html>
+    )
+}
