f38f0f5b332fc99ee6304a3df24ec65b478337fd
Sabare Victor Oketch <40304396+Sabareh@users.noreply.github.com>
2022-07-19 14:34:18 +0300

    dark

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,7 @@ import {Html, Head, Main, NextScript} from 'next/document'
 
 export default function Document() {
     return (
-        <Html classname="dark">
+        <Html className="dark">
             <Head>      
             </Head>
             <body>
